31b0c395bfe4f25b05d105b019ac6e08c3840694
okumin <git@okumin.com>
2020-12-13 22:38:14 +0900

    Ask about optional packages

--- docker/build-hadoop.sh
+++ docker/build-hadoop.sh
@@ -4,18 +4,26 @@ set -eu
 source ./docker/prepare.sh HADOOP_SOURCE_DIR
 source ./docker/prepare-maven.sh
 
+active_profiles=dist
+
+read -p "Build native libraries? [Y/n]: " native
+case "$native" in
+  y|Y|yes|"" ) active_profiles=${active_profiles},native;;
+  n|N|no ) ;;
+  * ) echo "Please input y or n"; exit 1;;
+esac
+
+read -p "Build YARN UI V2? [Y/n]: " yarn_ui_v2
+case "$yarn_ui_v2" in
+  y|Y|yes|"" ) active_profiles=${active_profiles},yarn-ui;;
+  n|N|no ) ;;
+  * ) echo "Please input y or n"; exit 1;;
+esac
+
 docker build \
   --tag ${DOCKER_IMAGE_NAME_PREFIX}/hadoop-build:${image_tag} \
   --file ${HADOOP_SOURCE_DIR}/dev-support/docker/Dockerfile \
   ${HADOOP_SOURCE_DIR}/dev-support/docker
-
-active_profiles=dist
-if [[ ${HADOOP_BUILD_NATIVE} == true ]]; then
-  active_profiles=${active_profiles},native
-fi
-if [[ ${HADOOP_BUILD_YARN_UI_V2} == true ]]; then
-  active_profiles=${active_profiles},yarn-ui
-fi
 
 docker build \
   --tag ${DOCKER_IMAGE_NAME_PREFIX}/hadoop-sandbox-hadoop:${image_tag} \
