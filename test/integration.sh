dc3785523b8781a35ad4bf4c1cd19c9c9b8840e2
okumin <git@okumin.com>
2021-01-03 02:11:41 +0900

    Auto-detect Hadoop/Tez versions to be tested

--- test/integration.sh
+++ test/integration.sh
@@ -2,20 +2,18 @@
 set -eu
 
 readonly test_dir=$(cd "$(dirname "$0")"; pwd)
-readonly hadoop_version=$1
-readonly tez_version=$2
 
 "${test_dir}/integration/divider.sh" "Test Web"
 "${test_dir}/integration/web.sh"
 
 "${test_dir}/integration/divider.sh" "Test Tez"
-"${test_dir}/integration/tez.sh" "${tez_version}"
+"${test_dir}/integration/tez.sh"
 
 "${test_dir}/integration/divider.sh" "Test Hive on Tez"
 "${test_dir}/integration/hive_on_tez.sh"
 
 "${test_dir}/integration/divider.sh" "Test MR"
-"${test_dir}/integration/mr.sh" "${hadoop_version}"
+"${test_dir}/integration/mr.sh"
 
 "${test_dir}/integration/divider.sh" "Test Hive on MR"
 "${test_dir}/integration/hive_on_mr.sh"
--- test/integration/mr.sh
+++ test/integration/mr.sh
@@ -3,21 +3,20 @@ set -eu
 
 readonly integration_dir=$(cd "$(dirname "$0")"; pwd)
 
-readonly hadoop_version=$1
-echo "Hadoop version = ${hadoop_version}"
 "${integration_dir}/divider.sh" "Start running a MR job"
 
 "${integration_dir}/run.sh" gohdfs rm -rf /user/zookage/mr-wordcount-input
 "${integration_dir}/run.sh" gohdfs rm -rf /user/zookage/mr-wordcount-output
 "${integration_dir}/run.sh" gohdfs put /etc/hosts /user/zookage/mr-wordcount-input
 
-"${integration_dir}/run.sh" \
+"${integration_dir}/run.sh" bash -c "
   hadoop \
   jar \
-  "/opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-${hadoop_version}.jar" \
+  /opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-*.jar \
   wordcount \
   /user/zookage/mr-wordcount-input \
   /user/zookage/mr-wordcount-output
+"
 "${integration_dir}/run.sh" gohdfs cat /user/zookage/mr-wordcount-output/part-r-00000
 
 "${integration_dir}/divider.sh" "Finished running a MR job"
