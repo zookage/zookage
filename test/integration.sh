8939656cdd403120d7c6ba8df9b18c59012fcac5
okumin <git@okumin.com>
2021-01-14 16:27:46 +0900

    Add Spark tests to a test suite

--- test/integration.sh
+++ test/integration.sh
@@ -18,6 +18,9 @@ readonly test_dir=$(cd "$(dirname "$0")"; pwd)
 "${test_dir}/integration/divider.sh" "Test Hive on MR"
 "${test_dir}/integration/hive_on_mr.sh"
 
+"${test_dir}/integration/divider.sh" "Test Spark"
+"${test_dir}/integration/spark.sh"
+
 "${test_dir}/integration/divider.sh" "Test ZooKeeper"
 "${test_dir}/integration/zookeeper.sh"
 
