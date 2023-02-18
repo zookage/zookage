e6e9be5c3c1231afdcfdc5206a779eba6b6bbf03
okumin <git@okumin.com>
2023-02-18 23:30:26 +0900

    Add HBase 2.5.3

--- docker/build-hbase.sh
+++ docker/build-hbase.sh
@@ -6,10 +6,13 @@ source ./docker/prepare.sh HBASE_SOURCE_DIR
 # shellcheck source=/mnt/docker/prepare-maven.sh
 source ./docker/prepare-maven.sh
 
+read -r -p "Hadoop version: " hadoop_three_version
+
 docker build \
   --tag "${DOCKER_IMAGE_NAME_PREFIX}/zookage-hbase:${image_tag}" \
   --build-arg "maven_image=${MAVEN_3_IMAGE}" \
   --build-arg "openjdk_image=${OPENJDK_8_IMAGE}" \
   --build-arg "clean=${clean}" \
+  --build-arg "hadoop_three_version=${hadoop_three_version}" \
   --file ./docker/hbase/Dockerfile \
   "${HBASE_SOURCE_DIR}"
