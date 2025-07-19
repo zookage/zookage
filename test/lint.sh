e61d794e1d3591386de6a745fbe5bbbc646212e3
Shohei Okumiya <okumin@apache.org>
2025-07-19 19:38:30 +0900

    Add license headers (#76)
    * Add license headers
    
    * License .licenserc.yaml
    
    * Fix zoo.cfg

--- test/lint.sh
+++ test/lint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -eu
 
 readonly test_dir=$(cd "$(dirname "$0")"; pwd)
@@ -14,3 +26,7 @@ echo "All shell scripts look good!"
 "${test_dir}/integration/divider.sh" "Lint YAML files"
 "${test_dir}/yamllint.sh"
 echo "All YAML files look good!"
+
+"${test_dir}/integration/divider.sh" "Lint license headers"
+"${test_dir}/license.sh"
+echo "All license headers look good!"
