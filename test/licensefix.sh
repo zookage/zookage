37dfad87c0755031f6fa72bbd3a7c5d2710bab85
okumin <okumin@apache.org>
2025-07-19 19:27:44 +0900

    Add license headers

--- test/licensefix.sh
+++ test/licensefix.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,8 +11,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-resources:
-- job.yaml
-commonLabels:
-  service: mkdir
-  tier: job
+set -eu
+
+readonly base_dir=$(dirname "$(cd "$(dirname "$0")" || exit; pwd)")
+
+docker run --rm -v "${base_dir}:/github/workspace" apache/skywalking-eyes:0.7.0 header fix
