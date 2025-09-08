d1fc2723b827969834b871398f6009baffc519ba
Shohei Okumiya <okumin@apache.org>
2025-09-08 17:45:06 +0900

    Prepare for Hadoop 3.4 (#88)

--- test/integration/container_warn.sh
+++ test/integration/container_warn.sh
@@ -21,24 +21,13 @@ readonly base_dir=$(dirname "$(dirname "${integration_dir}")")
 # shellcheck disable=SC2016
 "${base_dir}/bin/logs" | grep WARN \
   | grep -v 'WARNING: .* does not exist. Creating.' \
-  | grep -v 'pod/hdfs-namenode-.* conf.Configuration: No unit for' \
   | grep -v 'pod/hdfs-namenode-.* hdfs.DFSUtilClient: Namenode for .* remains unresolved for ID' \
   | grep -v 'pod/hdfs-namenode-.* ha.HealthMonitor: Transport-level exception trying to monitor health of NameNode' \
   | grep -v 'pod/hdfs-namenode-.* ha.ActiveStandbyElector: Ignoring stale result from old client with sessionId' \
   | grep -v 'pod/hdfs-namenode-.* ha.EditLogTailer: Edit log tailer interrupted' \
-  | grep -v 'pod/hdfs-datanode-.* conf.Configuration: No unit for' \
-  | grep -v 'pod/hdfs-datanode-.* datanode.DataNode: Slow BlockReceiver write data to disk cost' \
-  | grep -v 'pod/hdfs-datanode-.* datanode.DataNode: Slow PacketResponder send ack to upstream took' \
-  | grep -v 'pod/hdfs-datanode-.* impl.FsDatasetImpl: Lock held time above threshold' \
-  | grep -v 'pod/hdfs-datanode-.* datanode.DataNode: Slow BlockReceiver write packet to mirror took' \
-  | grep -v 'pod/hdfs-datanode-.* datanode.DataNode: Slow flushOrSync took' \
-  | grep -v 'pod/hdfs-datanode-.* datanode.DataNode: Problem connecting to server:' \
-  | grep -v 'pod/hdfs-datanode-.* ipc.Client: Address change detecte' \
-  `# HDFS-16347` \
-  | grep -v 'pod/hdfs-datanode-.* datanode.DirectoryScanner: dfs.datanode.directoryscan.throttle.limit.ms.per.sec set to value above 1000 ms/sec' \
-  | grep -v 'pod/hdfs-httpfs-.* log4j:WARN' \
-  | grep -v 'pod/hdfs-httpfs-.* \[SetPropertiesRule\]{Server/Service/Engine/Host} Setting property' \
-  | grep -v 'pod/hdfs-httpfs-.* Creation of SecureRandom instance for session ID generation using \[SHA1PRNG\] took' \
+  | grep -v 'pod/hdfs-datanode-.* impl.FsDatasetImpl: dfsUsed file missing in' \
+  | grep -v 'pod/hdfs-datanode-.* ipc.Client: Address change detected' \
+  | grep -v 'pod/hdfs-httpfs-.* impl.MetricsSystemImpl: httpfs metrics system already initialized!' \
   | grep -v 'pod/hdfs-journalnode-.* common.Storage: Storage directory .* does not exist' \
   | grep -v 'pod/hdfs-journalnode-.* server.JournalNodeSyncer: Journal at .* has no edit logs' \
   | grep -v 'pod/yarn-nodemanager-.* nodemanager.DefaultContainerExecutor: Exit code from container' \
