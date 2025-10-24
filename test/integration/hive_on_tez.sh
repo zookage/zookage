3338590d63346cd485ffdd0da28ac3526f5f1330
okumin <okumin@apache.org>
2025-10-24 11:20:36 +0900

    Update Ranger policies

--- test/integration/hive_on_tez.sh
+++ test/integration/hive_on_tez.sh
@@ -17,8 +17,7 @@ readonly integration_dir=$(cd "$(dirname "$0")"; pwd)
 
 "${integration_dir}/divider.sh" "Start running Hive on Tez queries"
 "${integration_dir}/run.sh" beeline -e "
-  DROP TABLE IF EXISTS mofu_tez;
-  CREATE TABLE mofu_tez (name string);
+  CREATE TABLE IF NOT EXISTS mofu_tez (name string);
   INSERT INTO mofu_tez (name) VALUES ('12345');
   SELECT name, count(1) FROM mofu_tez GROUP BY name;
 "
