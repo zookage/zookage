a849fdcb748fa2bc438fe48c146737dc5d78a3d2
okumin <okumin@apache.org>
2026-06-29 22:17:32 +0900

    Kerberize Ranger

---
+++ kubernetes/base/kdc/generate-keytabs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+readonly base_dir=$(cd "$(dirname "$0")"; pwd)
+readonly keytab_dir="${base_dir}/keytabs"
+readonly principals_file="${base_dir}/../common/config/kdc/principals.json"
+
+rm -f "${keytab_dir}"/*.keytab
+
+password_for_principal() {
+  local principal=$1
+  local service=${principal%%/*}
+
+  echo "${service%%@*}"
+}
+
+read_keytab_principals() {
+  jq -r '.principals[]
+    | select(.keytab != null)
+    | [.principal, .keytab]
+    | @tsv' "${principals_file}"
+}
+
+while IFS=$'\t' read -r principal keytab; do
+  principal_password=$(password_for_principal "${principal}")
+  ktutil -k "${keytab_dir}/${keytab}" add \
+    -p "${principal}" \
+    -w "${principal_password}" \
+    -e aes256-cts-hmac-sha1-96 \
+    -V 1
+  ktutil -k "${keytab_dir}/${keytab}" add \
+    -p "${principal}" \
+    -w "${principal_password}" \
+    -e aes128-cts-hmac-sha1-96 \
+    -V 1
+done < <(read_keytab_principals)
