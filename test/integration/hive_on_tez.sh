7ad6d57aeb31c39cc173f47a682e067136a1d3d7
okumin <okumin@apache.org>
2026-07-05 15:47:17 +0900

    Add S3 integration coverage for Hadoop jobs

--- test/integration/hive_on_tez.sh
+++ test/integration/hive_on_tez.sh
@@ -14,17 +14,59 @@
 set -eu
 
 readonly integration_dir=$(cd "$(dirname "$0")"; pwd)
+# shellcheck source=/mnt/test/integration/s3.sh
+source "${integration_dir}/s3.sh"
 
-"${integration_dir}/divider.sh" "Start running Hive on Tez queries"
-"${integration_dir}/run.sh" /bin/bash -lc 'TERM=dumb JAVA_TOOL_OPTIONS="-Djline.terminal=dumb -Dorg.jline.terminal.dumb=true" beeline -e "
-    CREATE TABLE IF NOT EXISTS mofu_tez (name string);
-    INSERT INTO mofu_tez (name) VALUES ('\''12345'\'');
-    SELECT name, count(1) FROM mofu_tez GROUP BY name;
-  "
-'
-"${integration_dir}/divider.sh" "Finished running Hive on Tez queries"
+run_beeline() {
+  local sql=$1
 
-echo "The test queries succeeded."
-echo
+  "${integration_dir}/run.sh" /bin/bash -lc "TERM=dumb JAVA_TOOL_OPTIONS=\"-Djline.terminal=dumb -Dorg.jline.terminal.dumb=true\" beeline -e \"${sql}\""
+}
+
+run_hive_on_tez_queries() {
+  local location_prefix=${1:-}
+  local name="Hive on Tez queries"
+  local success_message="The test queries succeeded."
+  local table="mofu_tez"
+  local location=""
+  local sql
+
+  if [[ -n "${location_prefix}" ]]; then
+    name="${name} on ${location_prefix}"
+    success_message="The S3 test queries succeeded on ${location_prefix}."
+  fi
+
+  "${integration_dir}/divider.sh" "Start running ${name}"
+
+  if [[ -n "${location_prefix}" ]]; then
+    location="${location_prefix}/${table}"
+    "${integration_dir}/run.sh" hadoop fs -rm -r -f "${location}"
+    sql="
+      DROP TABLE IF EXISTS ${table};
+      CREATE EXTERNAL TABLE ${table} (name string)
+      LOCATION '${location}';
+      INSERT INTO ${table} (name) VALUES ('12345');
+      SELECT name, count(1) FROM ${table} GROUP BY name;
+    "
+  else
+    sql="
+      DROP TABLE IF EXISTS ${table};
+      CREATE TABLE ${table} (name string);
+      INSERT INTO ${table} (name) VALUES ('12345');
+      SELECT name, count(1) FROM ${table} GROUP BY name;
+    "
+  fi
+
+  run_beeline "${sql}"
+  "${integration_dir}/divider.sh" "Finished running ${name}"
+
+  echo "${success_message}"
+  echo
+}
+
+run_hive_on_tez_queries
+
+ensure_s3_bucket test
+run_hive_on_tez_queries s3a://test/hive-on-tez
 
 "${integration_dir}/tez_log.sh"
