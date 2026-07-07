65930d36cae882cddac11d62c7bfbd1c417f939f
okumin <okumin@apache.org>
2026-07-08 00:09:27 +0900

    Relax yamllint image pin

--- test/yamllint.sh
+++ test/yamllint.sh
@@ -15,4 +15,4 @@ set -eu
 
 readonly base_dir=$(dirname "$(cd "$(dirname "$0")" || exit; pwd)")
 
-docker run --rm -v "${base_dir}:/data" cytopia/yamllint:1.23 --strict .
+docker run --rm -v "${base_dir}:/data" cytopia/yamllint:1 --strict .
